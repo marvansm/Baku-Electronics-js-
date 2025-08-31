4fd260c67088408a8deb8f13927e5adddff9c049
Marvan <marvansm@code.edu.az>
2025-09-01 02:41:00 +0400

    fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import bannerFactory from "./components/banner";
 import swiperVariants from "./common/swiper";
+import Header from "./layout/header";
 
 bannerFactory();
+Header();
 swiperVariants("mySwiper", 1, 0, 3000);
